83be46e5d4d3c5a9becfebfc51935a0285a4a0f6
Pablo Vergara <vergara.pablo0616@gmail.com>
2024-05-20 22:26:48 -0400

    ios tests for alert views

---
+++ tests/ios/test_alert_views.py
@@ -0,0 +1,60 @@
+import pytest
+from conftest_ios import test_setup_ios
+from screens.iOS.homeScreen import HomeScreen as onHomeScreen
+from screens.iOS.alertViewsScreen import AlertViewsScreen as onAlertViewsScreen
+
+
+"""
+IOS APP Alert Views
+"""
+
+
+@pytest.mark.usefixtures('test_setup_ios')
+class TestAlertViews:
+    """ALERTS"""
+    
+    def test_simple_alert_view(self):
+        onHomeScreen.select_option(self, 'Alert Views')
+        onAlertViewsScreen.select_alert_view(self, 'Simple')
+        onAlertViewsScreen.confirm_simple_alert
+        onAlertViewsScreen.click_on_alert_action(self, "OK")
+        
+    def test_ok_cancel_alert_view(self):
+        onHomeScreen.select_option(self, 'Alert Views')
+        onAlertViewsScreen.select_alert_view(self, 'Okay / Cancel')
+        onAlertViewsScreen.click_on_alert_action(self, "Cancel")       
+        onAlertViewsScreen.select_alert_view(self, 'Okay / Cancel')
+        onAlertViewsScreen.click_on_alert_action(self, 'OK')
+
+    def test_other_alert_view(self):
+        onHomeScreen.select_option(self, 'Alert Views')
+        
+        # ALERT - CHOICE ONE
+        onAlertViewsScreen.select_alert_view(self, 'Other')
+        onAlertViewsScreen.click_on_alert_action(self, 'Choice One') 
+        
+        # ALERT - CHOICE TWO
+        onAlertViewsScreen.select_alert_view(self, 'Other')
+        onAlertViewsScreen.select_alert_view(self, 'Choice Two')
+        
+        # ALERT - CANCEL CHOICES
+        onAlertViewsScreen.select_alert_view(self, 'Other')
+        onAlertViewsScreen.select_alert_view(self, 'Cancel')
+        
+    #TODO - Fix this test, fails due to input not being found by xpath
+    def test_text_entry_alert_view(self):
+        onHomeScreen.select_option(self, 'Alert Views')
+        onAlertViewsScreen.select_alert_view(self, 'Text Entry')
+        onAlertViewsScreen.submit_text_in_alert_input(self, 'this is my ios appium test')
+        onAlertViewsScreen.click_on_alert_action(self, 'OK')
+    
+    # def test_secure_text_entry_alert_view(self):
+    #     pass
+    
+    # """ACTION SHEETS"""
+    
+    # def test_confirm_cancel_alert_view(self):
+    #     pass
+    
+    # def test_destructive_alert_view(self):
+    #     pass
